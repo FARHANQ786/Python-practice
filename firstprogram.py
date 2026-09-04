dbe92de3194ffd236e868b0cbc4ff7b331341832
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-04 16:20:36 +0530

    first commit

---
+++ firstprogram.py
@@ -0,0 +1,78 @@
+name = "rockstar"
+age = 3
+price = 25.023
+p = False
+a = None
+print(type(name))
+print(type(age))
+print(type(price ))
+print(type(p))
+print(type(a))
+#summation of 2 numbers 
+a = 4
+b = 3
+sum = a+b
+print(sum)
+#subtraction of 2 numbers
+a = 34
+b = 34
+sub=a-b
+print(sub)
+# expression execution
+A,B=2,3
+Txt="@"
+print(2*Txt*3)
+# expression execution
+A,B="2",3
+Txt="@"
+print((A+Txt)*4)
+#
+A,B=2,3
+c=44
+print(A+B*c)
+#
+A,b=33,89.0
+c=A*b
+print(c)
+# // divides and converts integer value into smallest float , 
+# / divides and gives accurate answer
+a,b=1.5,3
+c=a//b
+print(c,a/b)
+# floor gives closest integer which is lesser than or equal to float value
+#result of floor(a/b) same as (a//b)
+a,b=12,5
+# remainder is negative when denominator is negative
+a,b=5,-2
+c=a%b
+print(c)
+# this is a single line comment which python is ignores
+""""this is a multi line comment
+
+
+"""
+# taking input in python
+# string input
+name = input("name : ")
+# int input
+age =int(input("age;"))
+#float input
+price = float(input("price : "))
+print("my name is",name,"my age is",age)
+# program to input 2 number and print their sum
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+c=a+b
+print("the sum of 2 numbers is : ",c)
+# side of s
+#not>and>or
+# and operator kehta hai 1 value bhi false toh answer false
+# or operartor kehta hai ki 1 value bhi true toh answer bhi true
+''' AND  - TT-T
+         FT-F
+         TF-False
+         FF-F
+   OR    - TT-T
+          TF-True
+          FT-True
+          FF-False'''
