582381aa3b60a6a16a6e01764c52de0a40cbf95f
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-04 17:15:16 +0530

    first commit

---
+++ 2 day program.py
@@ -0,0 +1,32 @@
+# conditional statements
+# if elif slse syantax
+''''if(condition):
+    statement1
+elif(condition): we can put n elif statement
+    statement2
+else:
+    staementn'''
+#9age=int(input("enter your age :"))
+
+'''if(age<10):
+    print("eleigible")
+elif(age==10): # important 2 equal bar
+    print("no")
+else:
+    print("yes")'''
+
+'''marks=int(input("enter your marks :"))
+if(marks>=90):
+    print("A")
+elif(90>marks>=80):
+    print("b")  
+elif(80>marks>=70):
+    print("c")
+else:
+    print("f")'''
+# SINGLE LINE IF/TERNARY OPERATOR
+#<VAR>=<VAL1>IF<CONDITION>ELSE<VAL2> 
+Food=input("enter your dish : ")
+eat="yes" if Food=="cake" else "no" # double equal important
+print(eat)    
+        
---
+++ SUM OF 2 NUMBERS.py
@@ -0,0 +1,4 @@
+x=int(input("enter your number : "))
+y=int(input("enter your second number : "))
+z=x+y
+print(f"the sum of {x} and {y} is {z}")
