c3656c41f6a2843e1c7fe2b3532576c464e78f65
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-05 13:35:19 +0530

    Update Python files

---
+++ clever if conditional statement.py
@@ -0,0 +1,4 @@
+age=int(input("enter your age :"))
+#<var>= (false-value,true-value) [condition]
+vote=("no","yes")[age>=18]
+print(vote)
---
+++ print tax of salary by clever if statements.py
@@ -0,0 +1,5 @@
+x=float(input("enter your salary :"))
+tax=x*(0.1,0.2)[x>=500000]
+w=x-tax
+print("your your tax is :",tax)
+print("your your salary after tax is :",w)
