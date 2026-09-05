f64df8112b32a278bb7ec927bb3a15953d2721ce
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-05 13:22:12 +0530

    Update Python files

---
+++ single line conditional sttements.py
@@ -0,0 +1,6 @@
+'''food = input("enter your favorite food : ")
+eat = "yes " if food =="cake" else "no"
+print(eat)'''
+
+x=input("enter your food : ")
+print("sweet") if x=="cake" or x=="jalebi" else print("not sweet")
