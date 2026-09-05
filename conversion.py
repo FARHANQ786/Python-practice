87d3f2f3af283d66779540968106d7a3e0214aac
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-05 15:21:41 +0530

    update python files

---
+++ conversion.py
@@ -0,0 +1,13 @@
+#type conversion
+# a=10
+# b=20.5
+# print(a+b)# gives the floating value as float is more precise than int
+# a=10
+# b="2"
+# print(a+b)# this will give error as we cannot add int and str
+# type casting
+a=int("20")
+b=30
+print(a+b)
+c=str("30")
+print(a+c)# this will show error as we cannot add int and str
---
+++ operators.py
@@ -0,0 +1,36 @@
+#arithemetic operators
+# a=11
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)# always gives float value
+# print(a%b) # gives the remained (% modulo operartor)
+# print(a**b)
+# print(a//b)# gives the smallest integer value of the division
+# realtion operators
+#in maths a=b in pyhton a==b , in maths a not equal b , in pyhton a!=b
+# they alwass return boolean value True or False
+# a=50
+# b=40
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+#assignment operators
+num=10
+num+=5 # num=num+5
+print(num)
+num-=5 # num=num-5
+print(num)
+num*=5 # num=num*5
+print(num)
+num/=5 # num=num/5
+print(num) 
+num%=5 # num=num%5
+print(num)
+num**=5 # num=num**5
+print(num)
+ 
