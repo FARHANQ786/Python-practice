2ba66a52b0d8be8340b3795fec49a546cb7c7e10
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-07 10:09:34 +0530

    update python files

---
+++ nested if conditions.py
@@ -0,0 +1,13 @@
+#nesting if statements
+#if(condition):
+#    if(condition):
+#        print("statement1")
+age = 99
+
+if(age>=18):
+    if(age>=80):
+        print("you cant drive")
+    else:
+        print("you can drive")
+else:
+    print("you cant drive")        
