88b8f168b90b67da4417499e56e0c35473a80ec0
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-07 10:59:01 +0530

    update python files

---
+++ lists.py
@@ -0,0 +1,53 @@
+# # list # we can store elements of different data types in a list
+# marks = [90, 80, 70, 60, 50] # values separated by comma and enclosed in square brackets
+# print(type(marks)) # <class 'list'>
+# print(marks)
+# print(marks[0]) # 90
+# student=["rockstar", 20, 5.6, "male", True]
+# print(type(student[4]))
+# print(student[4])
+# # list are mutable we can change the values of list
+# print(student[0])
+# student[0]="rofg"
+# print(student)
+# print(len(student)) # gives the length of list
+# print(student[0:])
+# # list slicing
+# print(student[0:3]) # ['rofg', 20, 5.6
+
+
+# # methods in a list
+# list=[5,5,2,8,0,3]
+
+# list.append(6) # adds 6 at the end of list
+# print(list)
+
+
+# list.sort() # sorts the list in ascending order
+# print(list)
+
+
+# list.sort(reverse=True) # sorts the list in descending order
+# print(list)
+
+# list.reverse() # reverses the list
+# print(list)
+
+
+list1=["a","b","c"]
+
+# list1.sort(reverse=True) # sorts the list in descending order
+# print(list1)
+
+
+# list1.reverse() # reverses the list
+# print(list1)
+
+# list1.insert(1,"d") # inserts "d" at index 1
+# print(list1)
+
+# list1.remove("a") # removes the first occurrence of a from the list
+# print(list1)
+
+# list1.pop(1) # removes the element at index 1
+# print(list1)
