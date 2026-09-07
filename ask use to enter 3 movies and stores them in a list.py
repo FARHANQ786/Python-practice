31d332b1f21d579e9cb6500b0979c44b5503b634
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-07 11:28:22 +0530

    update python files

--- ask use to enter 3 movies and stores them in a list.py
+++ ask use to enter 3 movies and stores them in a list.py
@@ -1,5 +1,8 @@
 mov1=input("enter first movie name :")
 mov2=input("enter second movie name :")
 mov3=input("enter third movie name :")
-movies=[mov1,mov2,mov3]
+movies=[]
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
 print(movies)
