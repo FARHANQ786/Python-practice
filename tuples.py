a876cc3b8698ea051d024516ff0edfd92f215300
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-07 11:06:48 +0530

    update python files

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# tuples 
+#tup=(1,2,3,4,5,6,7,8,9)
+# print(type(tup)) # <class 'tuple'>  
+
+
+# print(tup[0]) # 1
+# print(tup[1]) # 2
+
+# tup1=(1)
+# print(type(tup1)) # <class 'int'>  # it is not a tuple because it has only one element  
+
+tuple=(2,1,3,1)
+
+print(tuple.index(2)) # returns the index of first occurrence of 1 in the tuple
+
+print(tuple.count(1)) # returns the number of occurrences of 1 in the tuple
