6b18ba3db5d5c4f208d7eabaf69645ffd9517c64
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-19 11:47:08 +0530

    update python files

---
+++ ask user to enter subject and marks in emoty dictionary.py
@@ -0,0 +1,12 @@
+s1=input("enter your subject :")
+m1=int(input("enter the marks of s1 :"))
+s2=input("enter your subject :")
+m2=int(input("enter the marks of s2 :"))
+s3=input("enter your subject :")
+m3=int(input("enter the marks of s3 :"))
+dict={
+    s1:m1,
+    s2:m2,
+    s3:m3
+}
+print("marks are ", dict)
