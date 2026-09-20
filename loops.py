e3776ed3eb9bda22bb908eda9a82b4a00316d32d
FARHANQ786 <farhanahmed11715@gmail.com>
2026-09-20 09:00:16 +0530

    update python files

--- loops.py
+++ loops.py
@@ -1,4 +1,10 @@
 count = 1
 while count <=5:
     print("apna college")
-    count+=1
+    count+=1 # count = count + 1
+
+    # to print numbers from 1 to 5 
+    i = 5
+    while i>=1:
+        print(i)
+        i = i-1
